d7f2f8f75aab9c498bab7f2447bb261ceaab7197
AntonyMike <standbyzero@gmail.com>
2017-04-16 19:06:38 +0800

    add RWD

--- js/msa.js
+++ js/msa.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
         delay: 50
     });
     $('.button-collapse').sideNav({
-        menuWidth: 800, // Default is 300
+        menuWidth: 300, // Default is 300
         edge: 'left', // Choose the horizontal origin
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
